b6de9c23dea1f0e0b94ea8ded2ad385060efe8d6
joohokim <give928@gmail.com>
2022-10-07 23:52:28 +0900

    feat: controller 기능 구현
    자동차 경주 게임 제어
    - 입출력
    - 사용자가 잘못된 값을 입력할 경우 에러 메시지를 출력 후 다시 입력

---
+++ src/main/java/racingcar/ui/RacingCarController.java
@@ -0,0 +1,50 @@
+package racingcar.ui;
+
+import racingcar.domain.*;
+import racingcar.dto.RecordDto;
+
+public class RacingCarController {
+    private final Movement movement = new NumberConditionMovement(new RandomNumberGenerator());
+
+    public void start() {
+        Racing racing = Racing.of(createEntry(), createLap());
+        RecordDto recordDto = racing.race(movement);
+        OutputView.showRecord(recordDto);
+    }
+
+    private Entry createEntry() {
+        Entry entry = null;
+        while (entry == null) {
+            entry = inputCarNames();
+        }
+        return entry;
+    }
+
+    private static Entry inputCarNames() {
+        try {
+            String carNames = InputView.inputCarNames();
+            return Entry.from(carNames);
+        } catch(IllegalArgumentException e) {
+            OutputView.error(e.getMessage());
+        }
+        return null;
+    }
+
+    private Lap createLap() {
+        Lap lap = null;
+        while (lap == null) {
+            lap = inputLap();
+        }
+        return lap;
+    }
+
+    private Lap inputLap() {
+        try {
+            String lapCount = InputView.inputLapCount();
+            return Lap.from(lapCount);
+        } catch(IllegalArgumentException e) {
+            OutputView.error(e.getMessage());
+        }
+        return null;
+    }
+}
